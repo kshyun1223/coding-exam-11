775f05066f4aa965754f6f8d29df0e22fc0dbef2
kshyun1223 <kshyun.dev@gmail.com>
2022-11-28 13:02:21 +0900

    끝

--- readme.md
+++ readme.md
@@ -1,5 +1,34 @@
 ### 요구사항
+- 주어진 txt 파일을 조립해서 html 형태로 응답하시오
 - html.txt = 헤드 + 바디
-  - 바디 = 헤더 + 메인 + 풋터
-
+- 바디 = 헤더 + 메인 + 풋터
 - html 파일로 만들 수도 있지만 이번엔 텍스트파일로 다 만들어놓고 나중에 파싱 해보자 (실무에서는 그렇게 많이 한다)
+
+### 파일 구성
+- head.txt
+```html
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+```
+
+- header.txt
+```html
+<ul><li>메뉴</li></ul>
+```
+
+- main.txt
+```html
+<div id="root">hello</div>
+```
+
+- footer.txt
+```html
+<footer>kdt 클래스</footer>
+```
+
+- body.txt
+```html
+<!-- 빈 파일 -->
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("test")
+import * as http from 'http';
+import * as fs from 'fs';
+
+http.createServer((req, res) => {
+  /* head */
+  const head = fs.readFileSync('contents/head.txt', 'utf8')
+
+  /* body */
+  const header = fs.readFileSync('contents/header.txt', 'utf8')
+  const main = fs.readFileSync('contents/main.txt', 'utf8')
+  const footer = fs.readFileSync('contents/footer.txt', 'utf8')
+  fs.writeFileSync('contents/body.txt', `${header}\n${main}\n${footer}`, 'utf8')
+  
+  const body = fs.readFileSync('contents/body.txt', 'utf8')
+  fs.writeFileSync('contents/html.txt', `${head}\n${body}`, 'utf8')
+
+  const html = fs.readFileSync('contents/html.txt', 'utf8')
+  console.log(html)
+  
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end(html)
+})
+.listen(8080, () => { // 서버 연결
+  console.log('8080번 포트에서 서버 대기 중입니다!');
+});
+
