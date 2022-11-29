ed54a7eac4a7e58d92002f98bdfee1504a5f2aff
kshyun1223 <kshyun.dev@gmail.com>
2022-11-29 14:29:48 +0900

    조립 할 수 있겠다

--- dist/index.js
+++ dist/index.js
@@ -10,25 +10,12 @@ const server = http.createServer((req, res) => {
                 (() => {
                     /* assemble html */
                     (() => {
-                        const data = [];
-                        fs.readdir("./raw-data", (err, fileNameElement) => {
-                            if (err)
-                                throw err;
-                            console.log(fileNameElement);
-                            data.push(fileNameElement);
-                        });
-                        console.log(data);
-                        // fileNameElement.map((fileName) => {
-                        //   console.log(fileName)
-                        // })
-                    })();
-                    /* assemble html */
-                    (() => {
-                        const head = fs.readFileSync('./raw-data/head.txt', 'utf8');
-                        const header = fs.readFileSync('./raw-data/header.txt', 'utf8');
-                        const main = fs.readFileSync('./raw-data/main.txt', 'utf8');
-                        const footer = fs.readFileSync('./raw-data/footer.txt', 'utf8');
-                        fs.writeFileSync('./contents/html.txt', `${head}\n${header}\n${main}\n${footer}`, 'utf8');
+                        const head = fs.readFileSync('./raw_data/head.txt', 'utf8');
+                        const header = fs.readFileSync('./raw_data/header.txt', 'utf8');
+                        const main = fs.readFileSync('./raw_data/main.txt', 'utf8');
+                        const footer = fs.readFileSync('./raw_data/footer.txt', 'utf8');
+                        const html = `${head}\n${header}\n${main}\n${footer}\n`;
+                        fs.writeFileSync('./contents/html.txt', html, 'utf8');
                     })();
                     /* response html */
                     (() => {
