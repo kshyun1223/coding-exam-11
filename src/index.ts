801a7f137a36d0acae1aa33890ddeeb77cbb0551
kshyun1223 <kshyun.dev@gmail.com>
2022-11-29 10:08:23 +0900

    try~catch 처리

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,38 @@ import * as fs from 'fs';
 const port = 8080;
 
 http.createServer((req, res) => {
-  /* assemble body */
-  (() => {
-    const header = fs.readFileSync('contents/header.txt', 'utf8')
-    const main = fs.readFileSync('contents/main.txt', 'utf8')
-    const footer = fs.readFileSync('contents/footer.txt', 'utf8')
-    fs.writeFileSync('contents/body.txt', `${header}\n${main}\n${footer}`, 'utf8')
-  })();
-  
-  /* assemble html */
-  (() => {
-    const head = fs.readFileSync('contents/head.txt', 'utf8');
-    const body = fs.readFileSync('contents/body.txt', 'utf8')
-    fs.writeFileSync('contents/html.txt', `${head}\n${body}`, 'utf8')
-  })();
+  try {
+    if (req.method === 'GET'){
+      if (req.url === '/'){
+        (() => {
+          /* assemble body */
+          (() => {
+            const header = fs.readFileSync('contents/header.txt', 'utf8');
+            const main = fs.readFileSync('contents/main.txt', 'utf8');
+            const footer = fs.readFileSync('contents/footer.txt', 'utf8');
+            fs.writeFileSync('contents/body.txt', `${header}\n${main}\n${footer}`, 'utf8');
+          })();
 
-  /* response html */
-  (() => {
-    const html = fs.readFileSync('contents/html.txt', 'utf8')
-    console.log(html)
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end(html)
-  })();
+          /* assemble html */
+          (() => {
+            const head = fs.readFileSync('contents/head.txt', 'utf8');
+            const body = fs.readFileSync('contents/body.txt', 'utf8');
+            fs.writeFileSync('contents/html.txt', `${head}\n${body}`, 'utf8');
+          })();
+
+          /* response html */
+          (() => {
+            const html = fs.readFileSync('contents/html.txt', 'utf8');
+            console.log(html);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(html);
+          })();
+        })();
+      }
+    }
+  } catch (err) {
+    
+  }
 })
 .listen(port, () => { // 서버 연결
   console.log(`http://localhost:${8080}/`);
