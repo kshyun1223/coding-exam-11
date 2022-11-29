cd07292f00388d84a5016c6a4ce84189e117dd96
kshyun1223 <kshyun.dev@gmail.com>
2022-11-29 13:03:29 +0900

    raw-data 폴더의 파일 목록 출력

--- dist/index.js
+++ dist/index.js
@@ -10,15 +10,26 @@ const server = http.createServer((req, res) => {
                 (() => {
                     /* assemble html */
                     (() => {
-                        const head = fs.readFileSync('raw-data/head.txt', 'utf8');
-                        const header = fs.readFileSync('raw-data/header.txt', 'utf8');
-                        const main = fs.readFileSync('raw-data/main.txt', 'utf8');
-                        const footer = fs.readFileSync('raw-data/footer.txt', 'utf8');
-                        fs.writeFileSync('contents/html.txt', `${head}\n${header}\n${main}\n${footer}`, 'utf8');
+                        fs.readdir("./raw-data", (err, fileNameElement) => {
+                            // let data:[] = [];
+                            // if (err) throw err;
+                            console.log(fileNameElement);
+                            // fileNameElement.map((fileName) => {
+                            //   console.log(fileName)
+                            // })
+                        });
+                    })();
+                    /* assemble html */
+                    (() => {
+                        const head = fs.readFileSync('./raw-data/head.txt', 'utf8');
+                        const header = fs.readFileSync('./raw-data/header.txt', 'utf8');
+                        const main = fs.readFileSync('./raw-data/main.txt', 'utf8');
+                        const footer = fs.readFileSync('./raw-data/footer.txt', 'utf8');
+                        fs.writeFileSync('./contents/html.txt', `${head}\n${header}\n${main}\n${footer}`, 'utf8');
                     })();
                     /* response html */
                     (() => {
-                        const html = fs.readFileSync('contents/html.txt', 'utf8');
+                        const html = fs.readFileSync('./contents/html.txt', 'utf8');
                         console.log(html);
                         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
                         res.end(html);
