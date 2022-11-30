407624f407d0f6334694aea7bc3f2ac52bda05ec
kshyun1223 <kshyun.dev@gmail.com>
2022-11-30 11:58:06 +0900

    파일목록을 만들고, 가져오고, 배열로 만들고, 배열인 거 확인

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,56 @@
-import * as http from 'http';
-import * as fs from 'fs';
-
+import http = require('http');
+import fs = require('fs');
 const port = 8080;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req: any, res: any) => {
   try {
     if (req.method === 'GET'){
       if (req.url === '/'){
         (() => {
-          /* assemble html */
+
+          /* make fileList */
           (() => {
             fs.readdir("./raw_data", (err, file) => {
               if (err) throw err;
               const logJson = JSON.stringify(file, null, 2);
               fs.writeFileSync("./contents/log.json", logJson);
-            });
+            })
+          
+            const jsonFile = fs.readFileSync('./contents/log.json', 'utf8')
+            const jsonData = JSON.parse(jsonFile)
+            // console.log(jsonData) // [ 'footer.txt', 'head.txt', 'header.txt', 'main.txt' ]
+            // console.log(typeof(jsonData))// object
+
+            const fileArray = []
+
+            for (let key in jsonData){
+              const value = jsonData[key]
+              console.log(value)
+              fileArray.push(value)
+            }
+
+            console.log(fileArray)
+            console.log(Array.isArray(fileArray)) // true
+
             
-            const head = fs.readFileSync('./raw_data/head.txt', 'utf8');
-            const header = fs.readFileSync('./raw_data/header.txt', 'utf8');
-            const main = fs.readFileSync('./raw_data/main.txt', 'utf8');
-            const footer = fs.readFileSync('./raw_data/footer.txt', 'utf8');
-
-            const html = `${head}\n${header}\n${main}\n${footer}\n`
-
-            fs.writeFileSync('./contents/html.txt', html, 'utf8');
           })();
+
+          // const eachContents = {}
+            
+            // fileList.head = fs.readFileSync('./raw_data/head.txt', 'utf8');
+            // fileList.header = fs.readFileSync('./raw_data/header.txt', 'utf8');
+            // fileList.main = fs.readFileSync('./raw_data/main.txt', 'utf8');
+            // fileList.footer = fs.readFileSync('./raw_data/footer.txt', 'utf8');
+
+            // const html = `${head}\n${header}\n${main}\n${footer}\n`
+
+            // fs.writeFileSync('./contents/html.txt', html, 'utf8');
+          
 
           /* response html */
           (() => {
             const html = fs.readFileSync('./contents/html.txt', 'utf8');
-            console.log(html);
+            // console.log(html);
             res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
             res.end(html);
           })();
