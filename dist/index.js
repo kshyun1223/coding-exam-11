92f474d84c64979ef7bd3d00ced0bfe8bd1df628
kshyun1223 <kshyun.dev@gmail.com>
2022-12-01 16:43:51 +0900

    잡다한 파일들

--- dist/index.js
+++ dist/index.js
@@ -1,46 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
-const fs = require("fs");
-const querystring = require("querystring");
-const port = 8080;
+const main_1 = require("./main");
+const post_1 = require("./post");
+const port = 5580;
 const server = http.createServer((req, res) => {
     try {
         if (req.method === 'GET') {
+            /* 시작페이지 */
             if (req.url === '/') {
-                (() => {
-                    /* logJSON */
-                    (() => {
-                        fs.readdir("./raw_data", (err, file) => {
-                            if (err)
-                                throw err;
-                            const logJson = JSON.stringify(file, null, 2);
-                            fs.writeFileSync("./contents/log.json", logJson);
-                        });
-                    })();
-                    /* make html */
-                    (() => {
-                        const jsonFile = fs.readFileSync('./contents/log.json', 'utf8');
-                        const jsonData = JSON.parse(jsonFile);
-                        const fileArray = [];
-                        for (let key in jsonData) {
-                            const value = jsonData[key].replace('.txt', '');
-                            fileArray.push(value);
-                        }
-                        const eachContents = {};
-                        fileArray.forEach((key) => eachContents[key] = fs.readFileSync('./raw_data/' + `${key}` + '.txt', 'utf8'));
-                        // console.log(eachContents)
-                        const html = `${eachContents.head}\n${eachContents.header}\n${eachContents.main}\n${eachContents.footer}\n`;
-                        fs.writeFileSync('./contents/html.txt', html, 'utf8');
-                    })();
-                    /* response html */
-                    (() => {
-                        const html = fs.readFileSync('./contents/html.txt', 'utf8');
-                        // console.log(html);
-                        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-                        res.end(html);
-                    })();
-                })();
+                (0, main_1.main)(req, res);
+                /* 라우팅 */
             }
             else {
                 res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
@@ -48,16 +18,10 @@ const server = http.createServer((req, res) => {
             }
         }
         else if (req.method === 'POST') {
-            let body = '';
-            req.on('data', (data) => {
-                body += data;
-                const parsed = querystring.parse(body);
-                // console.log(parsed.content)
-                const component = `<p>${parsed}<p>`;
-                fs.writeFileSync("./raw_data/main.txt", component);
-            });
-            res.writeHead(302, { Location: '/' }); // 리다이렉트 하려면 302 코드 필요한듯...?
-            res.end();
+            /* 포스트 입력 */
+            if (req.url === '/post') {
+                (0, post_1.post)(req, res);
+            }
         }
     }
     catch (err) {
@@ -65,5 +29,5 @@ const server = http.createServer((req, res) => {
     }
 });
 server.listen(port, () => {
-    console.log(`http://localhost:${8080}/`);
+    console.log(`http://localhost:${port}/`);
 });
