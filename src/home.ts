9633cda8b9857e953d6255ff5a48866e8feea656
kshyun1223 <kshyun.dev@gmail.com>
2022-12-06 17:03:54 +0900

    일단 비동기 처리 하고나서 더 잘게 나눌거라 일단 합쳐놓음

--- src/home.ts
+++ src/home.ts
@@ -1,43 +1,43 @@
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
-export const home = (_req:any, res:any):void => {
-  (() => {
-    /* logJSON */
-    (()=> {
-      fs.readdir("./raw_data", (err, file) => {
-        if (err) throw err;
-        const logJson = JSON.stringify(file, null, 2);
-        fs.writeFileSync("./contents/log.json", logJson);
-      })
-    })();
+// export const home = (_req:any, res:any):void => {
+//   (() => {
+//     /* logJSON */
+//     (()=> {
+//       fs.readdir("./raw_data", (err, file) => {
+//         if (err) throw err;
+//         const logJson = JSON.stringify(file, null, 2);
+//         fs.writeFileSync("./contents/log.json", logJson);
+//       })
+//     })();
 
-    /* make html */
-    (() => {
-      const jsonFile = fs.readFileSync('./contents/log.json', 'utf8')
-      const jsonData = JSON.parse(jsonFile)
+//     /* make html */
+//     (() => {
+//       const jsonFile = fs.readFileSync('./contents/log.json', 'utf8')
+//       const jsonData = JSON.parse(jsonFile)
 
-      const fileArray = []
-      for (let key in jsonData){
-        const value = jsonData[key].split('.')
-        fileArray.push(value[0])
-      }
-      // console.log(fileArray) //['footer', 'head', 'header', 'main']
+//       const fileArray = []
+//       for (let key in jsonData){
+//         const value = jsonData[key].split('.')
+//         fileArray.push(value[0])
+//       }
+//       // console.log(fileArray) //['footer', 'head', 'header', 'main']
 
-      const eachContents:any = {}
-      fileArray.forEach((key) => eachContents[key] = fs.readFileSync(`./raw_data/${key}`+".txt", 'utf8'))
-      // console.log("이게모얌?", eachContents)
-      // console.log("이건모얌?", JSON.parse(eachContents.main))
+//       const eachContents:any = {}
+//       fileArray.forEach((key) => eachContents[key] = fs.readFileSync(`./raw_data/${key}`+".txt", 'utf8'))
+//       // console.log("이게모얌?", eachContents)
+//       // console.log("이건모얌?", JSON.parse(eachContents.main))
 
-      const html = `${eachContents.head}\n${eachContents.header}\n${eachContents.main}\n${eachContents.footer}\n`
-      fs.writeFileSync('./contents/html.txt', html, 'utf8');
-    })();
+//       const html = `${eachContents.head}\n${eachContents.header}\n${eachContents.main}\n${eachContents.footer}\n`
+//       fs.writeFileSync('./contents/html.txt', html, 'utf8');
+//     })();
 
-    /* response html */
-    (() => {
-      const html = fs.readFileSync('./contents/html.txt', 'utf8');
-      // console.log(html);
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-      res.end(html);
-    })();
-  })();
-}
+//     /* response html */
+//     (() => {
+//       const html = fs.readFileSync('./contents/html.txt', 'utf8');
+//       // console.log(html);
+//       res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+//       res.end(html);
+//     })();
+//   })();
+// }
--- src/post.ts
+++ src/post.ts
@@ -1,36 +1,35 @@
-import * as querystring from 'querystring';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
-export const post = (req:any, res:any):void => {
-  let body =''
-  req.on('data', (data: string) => {
-    body+=data
-    const parsed = querystring.parse(body)
+// export const post = (req:any, res:any):void => {
+//   let body =''
+//   req.on('data', (data: string) => {
+//     body+=data
+//     const parsed = querystring.parse(body)
     
-    interface MainType {
-      date: Date;
-      title: string | string[] ;
-      content: string | string[];
-    }
-    class Main implements MainType {
-      constructor (
-        public date: Date,
-        public title: string | string[],
-        public content: string | string[],
-      ) { }
-    }
-    const main = new Main(new Date(), parsed.title, parsed.content);
+//     interface MainType {
+//       date: Date;
+//       title: string | string[] ;
+//       content: string | string[];
+//     }
+//     class Main implements MainType {
+//       constructor (
+//         public date: Date,
+//         public title: string | string[],
+//         public content: string | string[],
+//       ) { }
+//     }
+//     const main = new Main(new Date(), parsed.title, parsed.content);
     
-    const mainAssemble = `
-      <p>날짜: ${main.date.toLocaleString()}</p>
-      <p>제목: ${main.title}</p>
-      <p>내용: ${main.content}</p>
-    `
-    console.log(mainAssemble)
+//     const mainAssemble = `
+//       <p>날짜: ${main.date.toLocaleString()}</p>
+//       <p>제목: ${main.title}</p>
+//       <p>내용: ${main.content}</p>
+//     `
+//     console.log(mainAssemble)
 
-    fs.writeFileSync("./raw_data/main.txt", mainAssemble);
-  });
+//     fs.writeFileSync("./raw_data/main.txt", mainAssemble);
+//   });
 
-  res.writeHead(302, {Location: '/'}); // 리다이렉트 하려면 302 코드 필요한듯...?
-  res.end();
-}
+//   res.writeHead(302, {Location: '/'}); // 리다이렉트 하려면 302 코드 필요한듯...?
+//   res.end();
+// }
